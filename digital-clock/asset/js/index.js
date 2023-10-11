ca56dd6ef61eace03f88c23397eb9281ba0fcc40
JackOwlTV <dylan.vivant@sfr.fr>
2023-10-11 16:43:28 +0200

    Modification du JS

--- digital-clock/asset/js/index.js
+++ digital-clock/asset/js/index.js
@@ -6,14 +6,14 @@ const dot = document.querySelector('.dot');
 
 setInterval(() => {
     let date = new Date();
-    let hr = (date.getHours() % 12) * 30; // Utilisez % 12 pour obtenir l'heure au format 12 heures
-    let min = date.getMinutes() * 6;
-    let sec = date.getSeconds() * 6;
+    let hr = (date.getHours() / 12) * 360; // Utilisez % 12 pour obtenir l'heure au format 12 heures
+    let min = (date.getMinutes() / 60) * 360;
+    let sec = (date.getSeconds() / 60) * 360;
 
 
-    hour.style.transform = `translate(-50%, -100%) rotateZ(${(hr / 2.4)}deg) translate(0, 100px)`;
-    minute.style.transform = `translate(-50%, -100%) rotateZ(${(min / 2)}deg) translate(0, 100px)`;
-    second.style.transform = `translate(-50%, -100%) rotateZ(${(sec)}deg) translate(0, 100px)`;
+    hour.style.transform = `translate(-50%, -100%) rotate(${(hr)}deg) translate(0, 100px)`;
+    minute.style.transform = `translate(-50%, -100%) rotate(${(min)}deg) translate(0, 100px)`;
+    second.style.transform = `translate(-50%, -100%) rotate(${(sec)}deg) translate(0, 100px)`;
 
     for (var i = 0; i < numbers.length; i++) {
         numbers[i].classList.remove('active');
@@ -24,5 +24,4 @@ setInterval(() => {
     } else {
         numbers[date.getHours() % 12 - 1].classList.add('active'); // Sinon, ajoutez la classe "active" à l'index correspondant à l'heure au format 12 heures
     }
-
 }, 1000);
